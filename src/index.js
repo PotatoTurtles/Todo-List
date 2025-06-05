8aaaed812c46312bd98a7967132ff98ded4ca54a
PotatoTurtles <ty.jones.0820@gmail.com>
2025-06-05 12:03:15 -1100

    Make Webpack Work Properly and Add Webpage Layout
    This commit adds the basic layout for the Todo list. It also erases an
    issue with webpack type=commonjs

---
+++ src/index.js
@@ -0,0 +1,26 @@
+//import { compareAsc, format } from "date-fns";
+
+import "./style.css";
+
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      e.name === "QuotaExceededError" &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
+storageAvailable("localStorage");
+console.log("hello world");
