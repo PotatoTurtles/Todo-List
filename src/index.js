ff4a02372ea1f56e3d20491de8d271df0005fad5
PotatoTurtles <ty.jones.0820@gmail.com>
2025-06-08 07:04:01 -1100

    Make Projects Expand and Create Center Form
    This commit is mainly focused on adding functionality to the Projects
    displayed on the sidebar. Some things that are included are the
    expanding and retracting buttons added as well as the show more and
    show less buttons. Both of these were made so that they don't have to
    be fed a link to a specific element and instead navigate to it on their
    own which will become helpful later.
    
    The center form has also been created with some relatively simplistic
    CSS applied. This will be changed more in the future.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//import { compareAsc, format } from "date-fns";
+import { compareAsc, format } from "date-fns";
 
 import "./style.css";
 
@@ -24,3 +24,51 @@ function storageAvailable(type) {
 
 storageAvailable("localStorage");
 console.log("hello world");
+
+let userStorage = [];
+
+const allExpand = [...document.querySelectorAll('.projectTitle>button')];
+allExpand.forEach((val)=>val.addEventListener('click',()=>{
+    val.textContent=='^'?val.textContent='v':val.textContent='^';
+    choosePop(val.parentNode);
+}));
+
+
+const allPop = [...document.querySelectorAll(".right>button")];
+allPop.forEach((val)=>val.addEventListener('click',()=>{val.textContent=='–'?val.textContent='+':val.textContent='–'}));//val.textContent=='-'?val.textContent='+':val.textContent='+'
+
+function hideChildren(self){
+    const hide = [...self.parentNode.querySelectorAll(".taskTile")];
+    hide.forEach((val=>val.style.display='none'));
+}
+function showChildren(self){
+    console.log("made it!");
+    const hide = [...self.parentNode.querySelectorAll(".taskTile")];
+    hide.forEach((val=>val.style.display='flex'));
+}
+
+function checkDisplay(self){
+    
+    const children = [...self.parentNode.querySelectorAll('.taskTile')];
+    let flag = false
+    children.forEach((kid)=>{
+        console.log(kid.style.display);
+        flag = kid.style.display=='none'
+    });
+    return flag
+}
+
+function choosePop(self){
+    // console.log(checkDisplay(self));
+    if(checkDisplay(self)){
+        showChildren(self);
+    }
+    else{
+        hideChildren(self);
+    }
+    //checkDisplay(self)?showChildren(self):;
+    
+}
+
+//hideChildren(document.querySelector('.projectTitle'));
+// showChildren(document.querySelector('.projectTitle'));
