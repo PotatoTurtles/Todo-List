a4542497ade08f48a59c904c9f59a3db18fdcd1d
PotatoTurtles <ty.jones.0820@gmail.com>
2025-06-08 11:07:58 -1100

    Add popToForm and unpopForm to index.js
    This commit adds the ability to "pop" information from the sidebar
    into the center form to be editable for the user. Also added with "unpop"
    is the ability to delete the previous data saved to storage and replace
    it with the altered content.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import { compareAsc, format } from "date-fns";
 
 import "./style.css";
 
+const inTitle = document.querySelector("input#title");
+const date = document.querySelector("input#date");
+const radio = [...document.querySelectorAll("input.rad")];
+const area = document.querySelector("textarea");
+const sub = document.querySelector(".create");
+const radIn = document.querySelector("#newInput");
+
+sub.addEventListener('click',()=>console.log(date.value));
+
+
 function storageAvailable(type) {
   let storage;
   try {
@@ -25,7 +35,21 @@ function storageAvailable(type) {
 storageAvailable("localStorage");
 console.log("hello world");
 
-let userStorage = [];
+let userStorage = {
+    "userData":{
+        username:"User", 
+        tasksCompleted: 0,
+    },
+    "userProjects":{
+        "Project Title":{
+            "Title":{
+                "due":"2025-08-20",
+                "projName":"Project Title",
+                "description":"Idk u gotta do work homi pls. Ur finna get fired atp!"
+            }
+        }
+    }
+};
 
 const allExpand = [...document.querySelectorAll('.projectTitle>button')];
 allExpand.forEach((val)=>val.addEventListener('click',()=>{
@@ -35,7 +59,16 @@ allExpand.forEach((val)=>val.addEventListener('click',()=>{
 
 
 const allPop = [...document.querySelectorAll(".right>button")];
-allPop.forEach((val)=>val.addEventListener('click',()=>{val.textContent=='–'?val.textContent='+':val.textContent='–'}));//val.textContent=='-'?val.textContent='+':val.textContent='+'
+allPop.forEach((val)=>val.addEventListener('click',()=>{
+    if(val.textContent=='–'){
+        val.textContent='+';
+        unpopForm(val);
+    }
+    else{
+        val.textContent='–';
+        popToForm(val);
+    }
+}));
 
 function hideChildren(self){
     const hide = [...self.parentNode.querySelectorAll(".taskTile")];
@@ -58,17 +91,60 @@ function checkDisplay(self){
     return flag
 }
 
+function popToForm(self){
+    let taskTitle = self.closest(".taskTile").querySelector("h4");
+    let projTitle = self.closest(".project").querySelector(".projectTitle>h3");
+    let taskData = userStorage["userProjects"][projTitle.textContent][taskTitle.textContent];
+
+    inTitle.value=taskTitle.textContent;
+    date.value=taskData["due"];
+    radio.forEach((element)=>{
+        if(element.value==projTitle.textContent){
+            element.checked=true;
+        }
+        else{
+            element.checked=false;
+        }
+    });
+    area.value=taskData["description"]
+    
+}
+
+function unpopForm(self){
+    let taskTitle = self.closest(".taskTile").querySelector("h4");
+    let projTitle = self.closest(".project").querySelector(".projectTitle>h3");
+    delete userStorage["userProjects"][projTitle.textContent][taskTitle.textContent];
+
+    
+
+    let proj ="";
+    radio.forEach((element)=>{
+        if(element.checked){
+            proj=element.value
+        }
+    });
+
+    userStorage["userProjects"][proj] ??= {};
+    userStorage["userProjects"][proj][inTitle.value] ??= {};
+
+    userStorage["userProjects"][proj][inTitle.value]["due"]=date.value;
+    date.value="";
+    userStorage["userProjects"][proj][inTitle.value]["projName"]=proj;
+    userStorage["userProjects"][proj][inTitle.value]["description"]=area.value;
+    area.value="";
+    for(let i=0; i<radio.length;i++){
+        i==0?radio[i].checked=true:radio[i].checked=false;
+    }
+    inTitle.value="";
+    radIn.value="";
+}
+
 function choosePop(self){
-    // console.log(checkDisplay(self));
     if(checkDisplay(self)){
         showChildren(self);
     }
     else{
         hideChildren(self);
     }
-    //checkDisplay(self)?showChildren(self):;
-    
 }
 
-//hideChildren(document.querySelector('.projectTitle'));
-// showChildren(document.querySelector('.projectTitle'));
